3255cc8fc607bb43eb24ec703e9b9bd27293e0dc
caiochaim <caiochain@gmail.com>
2016-06-04 13:54:32 -0300

    conserto master

---
+++ js/controllers/angular-cadastro-controller_BACKUP_7836.js
@@ -0,0 +1,57 @@
+application.controller('cadastroController', ['$scope', 'cadastroService', 'mensagemFactory', function ($scope, cadastroService, mensagemFactory) {
+
+	var propriedades = {};
+
+	$scope.cadastrar = function (usuario)
+	{
+		if ($scope.cadastro.$invalid)
+		{
+			propriedades = {
+				titulo: "Ooops!",
+				mensagem: "Preencha todos os campos corretamente!"
+			};
+
+			mensagemFactory.erro(propriedades);
+
+			angular.forEach($scope.cadastro, function (value, key) 
+			{
+				if (typeof value === "object" && value.$invalid)
+					value.$setDirty();
+			});
+		}
+		else
+		{
+			cadastroService.salvar(usuario).then(function () 
+			{
+				$scope.limparCampos();
+				$('.close h1').click();
+				
+				propriedades = {
+					titulo: "Concluído!",
+					mensagem: "Um email foi enviado para confirmação! \n Por favor confira sua caixa de entrada."
+				};
+
+				mensagemFactory.sucesso(propriedades);
+			},
+			function () 
+			{
+				propriedades = {
+					titulo: "Ooops!",
+					mensagem: "Usuário já existe no sistema!"
+				};
+
+				mensagemFactory.erro(propriedades);
+			});
+		}
+	};
+
+	$scope.limparCampos = function ()
+	{
+		delete $scope.usuario;
+		delete $scope.confirmaEmail;
+		delete $scope.confirmaSenha;
+		$scope.cadastro.$setPristine();
+		$('.error-message').addClass('hide');
+	}
+
+}]);
