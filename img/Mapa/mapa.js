462b63461a1a889d0c3df9dde5fd0843d5daa0c7
rfassina <rhfassina@hotmail.com>
2016-07-16 12:22:55 -0300

    #7520 parte desenvolvida do maps

---
+++ img/Mapa/mapa.js
@@ -0,0 +1,130 @@
+var geocoder;
+var map;
+var marker;
+
+function initialize() {
+	var latlng = new google.maps.LatLng(-18.8800397, -47.05878999999999);
+	var options = {
+		zoom: 5,
+		center: latlng,
+		mapTypeId: google.maps.MapTypeId.ROADMAP
+	};
+
+	map = new google.maps.Map(document.getElementById("mapa"), options);
+
+	geocoder = new google.maps.Geocoder();
+
+	marker = new google.maps.Marker({
+		map: map,
+		draggable: true,
+	});
+
+	marker.setPosition(latlng);
+}
+
+$(document).ready(function () {
+
+	initialize();
+
+	function carregarNoMapa(endereco) {
+		geocoder.geocode({ 'address': endereco + ', Brasil', 'region': 'BR' }, function (results, status) {
+			if (status == google.maps.GeocoderStatus.OK) {
+				if (results[0]) {
+					var latitude = results[0].geometry.location.lat();
+					var longitude = results[0].geometry.location.lng();
+
+					$('#txtEndereco').val(results[0].formatted_address);
+					$('#txtLatitude').val(latitude);
+                   	$('#txtLongitude').val(longitude);
+
+					var location = new google.maps.LatLng(latitude, longitude);
+					marker.setPosition(location);
+					map.setCenter(location);
+					map.setZoom(16);
+				}
+			}
+		})
+	}
+
+	$("#btnEndereco").click(function() {
+		if($(this).val() != "")
+			carregarNoMapa($("#txtEndereco").val());
+	})
+
+	$("#txtEndereco").blur(function() {
+		if($(this).val() != "")
+			carregarNoMapa($(this).val());
+	})
+
+	google.maps.event.addListener(marker, 'drag', function () {
+		geocoder.geocode({ 'latLng': marker.getPosition() }, function (results, status) {
+			if (status == google.maps.GeocoderStatus.OK) {
+				if (results[0]) {
+					$('#txtEndereco').val(results[0].formatted_address);
+					$('#txtLatitude').val(marker.getPosition().lat());
+					$('#txtLongitude').val(marker.getPosition().lng());
+				}
+			}
+		});
+	});
+
+	$("#txtEndereco").autocomplete({
+		source: function (request, response) {
+			geocoder.geocode({ 'address': request.term + ', Brasil', 'region': 'BR' }, function (results, status) {
+				response($.map(results, function (item) {
+					return {
+						label: item.formatted_address,
+						value: item.formatted_address,
+						latitude: item.geometry.location.lat(),
+          				longitude: item.geometry.location.lng()
+					}
+				}));
+			})
+		},
+		select: function (event, ui) {
+			$("#txtLatitude").val(ui.item.latitude);
+    		$("#txtLongitude").val(ui.item.longitude);
+			var location = new google.maps.LatLng(ui.item.latitude, ui.item.longitude);
+			marker.setPosition(location);
+			map.setCenter(location);
+			map.setZoom(16);
+		}
+	});
+
+	$("form").submit(function(event) {
+		event.preventDefault();
+
+		var endereco = $("#txtEndereco").val();
+		var latitude = $("#txtLatitude").val();
+		var longitude = $("#txtLongitude").val();
+
+		alert("Endereço: " + endereco);
+	});
+
+});
+
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
