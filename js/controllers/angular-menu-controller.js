22ff9b7e2e1bee37a26f2706200d9b5d14e2a9d1
caiochaim <caiochain@gmail.com>
2016-10-04 00:03:25 -0300

    #8522 - Alterações no layout

--- js/controllers/angular-menu-controller.js
+++ js/controllers/angular-menu-controller.js
@@ -10,30 +10,37 @@ appoie.controller('menuController', ['$scope', 'menuFactory', function ($scope, 
 	$scope.filtros = [
 	    { nome: 'Abertos', wanted: false },
 	    { nome: 'Fechados', wanted: false },
-	    { nome: 'Minhas publicações', wanted: false },
-	    { nome: '10 mais apoiados', wanted: false }
+	    { nome: 'Minhas publicações', wanted: false }
 	];
-
-	$scope.categorias = menuFactory.categorias();
 
 	$('.span').on('click', function(event) {
 
-		if ($(this).next().hasClass('hide'))
+		if ($(this).next().hasClass('hide') && $(this).children().html() == 'add')
 		{
+			$(this).children().html('remove');
 			$(this).next().slideToggle();
 			$(this).next().removeClass('hide');
 		}
 		else
 		{
+			$(this).children().html('add');
 			$(this).next().slideToggle();
 			$(this).next().addClass('hide');
 		}
 		
 	});
 
-	$scope.selecionarCategoria = function (element)
-	{
-		element.removeAttribute('class');
-	}
+	$('.img-select').on('click', function(event) {
+
+		if ($(this).hasClass('img-disabled'))
+			$(this).removeClass('img-disabled');
+		else
+			$(this).addClass('img-disabled');
+
+		console.log($(this).attr("alt"));
+
+	});
+
+
 
 }]);
--- js/services/factories/angular-menu-factory.js
+++ js/services/factories/angular-menu-factory.js
@@ -5,27 +5,6 @@ appoie.factory('menuFactory', ['$mdSidenav', '$timeout', function ($mdSidenav, $
 		buildToggler: function (componentId)
 		{
 			return $mdSidenav(componentId).toggle();
-		},
-		
-		categorias: function ()
-		{
-			return [
-				[
-					{url: '/img/categorias/ARBORIZACAO.png', name: 'ARBORIZACAO'},
-					{url: '/img/categorias/DEFESACIVIL.png', name: 'DEFESACIVIL'},
-					{url: '/img/categorias/FUNDODEVALE.png', name: 'FUNDODEVALE'}
-				],
-				[
-					{url: '/img/categorias/ILUMINACAO.png', name: 'ILUMINACAO'},
-					{url: '/img/categorias/PAVIMENTACAO.png', name: 'PAVIMENTACAO'},
-					{url: '/img/categorias/SANEAMENTOBASICO.png', name: 'SANEAMENTOBASICO'}
-				],
-				[
-					{url: '/img/categorias/SEGURANCA.png', name: 'SEGURANCA'},
-					{url: '/img/categorias/TERRENOBALDIO.png', name: 'TERRENOBALDIO'},
-					{url: '/img/categorias/TRANSPORTEPUBLICO.png', name: 'TRANSPORTEPUBLICO'}
-				]
-			]
 		}
 	}
 
