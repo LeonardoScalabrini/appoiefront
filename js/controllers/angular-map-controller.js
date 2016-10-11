4a658350dab3102fd6bde9e2b5c08d5bd40ef2dd
caiochaim <caiochain@gmail.com>
2016-10-11 00:56:06 -0300

    #8522 - Customização da prévia de postagem

--- js/controllers/angular-map-controller.js
+++ js/controllers/angular-map-controller.js
@@ -155,53 +155,159 @@ appoie.controller('mapController', ['$scope', 'mapService', function ($scope, ma
 	      		
 	      		$scope.postMin = response.data;
 	      		idPublicacao = $scope.postMin.idPublicacao;
-	      		//console.log($scope.postMin);
+	      		console.log($scope.postMin);
 	      		//var teste = $scope.postMin.idPublicacao.toString();
 	      		//alert(teste);
 	      		//	var idPublicacao = $scope.postMin.idPublicacao.toString();
 	      		//teste = "apoiar('" + $scope.postMin.idPublicacao + "')";
-	      		
-	      		
 
 	      		var infowindow = new google.maps.InfoWindow({
-			    content: '<div align="center">'
+			    content: '<md-card id="iw-container" ng-controller="mapController">'
 
-			    		 + 		'<h3>' + $scope.postMin.titulo + '</h3>'
-			    		 + '</div>'
-			    		 +'<div align="center" >'
-			    		 + 		'<img src=  "' + $scope.postMin.foto + '" style="width:150px;"></img>'
-			    		 + '</div>'
+			    		+	'<div class="iw-title">'+ $scope.postMin.titulo +'</div>'
 
-			    		 +'<div class="footerInfo">'
-			    		 + 		'<p>'
-			    		 + 			'<a onclick="apoiar(idPublicacao)" style="cursor:pointer;float:left; text-decoration:none;">Apoiar</a>'
-			    		 //+ 			'<a onclick="apoiar(' + idPublicacao + ')" style="cursor:pointer;float:left; text-decoration:none;">Apoiar</a>'
-			    		 +			'<a href="#" style="float:right;color:black;">Ver mais</a>'
-			    		 + 		'</p>'
-			    		 +		'<p>'
-			    		 +			$scope.postMin.qtdApoiadores + 'apoiadores'
-			    		 +		'</p>'
-			    		 + '</div>' 			    		 
+			    		+	'<div class="iw-content">'
+			    		+		'<img class="img-publicacao" src="'+ $scope.postMin.foto +'" alt="">'
+			    		+ 	'</div>'
 
-				  });
+			    		+	'<div class="iw-footer">'
+			    		+		'<div layout="row">'
+
+			    		+			'<div flex class="apoiar">'
+
+			    		+				'<img src="/img/logo-apoiar.png">'
+			    		+				'<p>Apoiar</p>'
+			    		
+			    		+			'</div>'
+
+			    		+			'<div flex class="qtdApoiadores">'
+			    		+				'<p>'+ $scope.postMin.qtdApoiadores +' Apoiadores</p>'
+			    		+			'</div>'
+
+			    		+		'</div>'
+			    		+ 	'</div>'
+
+			    		+	'<div class="iw-btn-modal">'
+			    		+		'<div layout="row">'
+
+			    		+			'<div flex class="show-modal">'
+			    		+				'<md-button class="md-button md-raised md-primary">VER MAIS</md-button>'
+			    		+			'</div>'
+
+			    		+		'</div>'
+			    		+ 	'</div>'
+			    		+'</md-card>'		    		 
+
+				});
 				  
-				 marker.addListener('click', function() {
-		  	  infowindow.open($scope.map, marker);
-		  	});
-	      	}, function(response) {
+				marker.addListener('click', function() {
+				  	infowindow.open($scope.map, marker);
+				});
 
-	      	});
+				google.maps.event.addListener(infowindow, 'domready', function() {
+
+				   // Referência ao DIV que recebe o conteúdo da infowindow recorrendo ao jQuery
+				   var iwOuter = $('.gm-style-iw');
+
+				   /* Uma vez que o div pretendido está numa posição anterior ao div .gm-style-iw.
+				    * Recorremos ao jQuery e criamos uma variável iwBackground,
+				    * e aproveitamos a referência já existente do .gm-style-iw para obter o div anterior com .prev().
+				    */
+				   var iwBackground = iwOuter.prev();
+
+				   // Remover o div da sombra do fundo
+				   iwBackground.children(':nth-child(2)').css({'display' : 'none'});
+
+				   // Remover o div de fundo branco
+				   iwBackground.children(':nth-child(4)').css({'display' : 'none'});
+
+					iwBackground.children(':nth-child(3)').find('div').children().css({'box-shadow': 'rgba(72, 181, 233, 0.6) 0px 1px'});
+
+					// Aproveitando a referência já criada ao div .gm-style-iw com a variável iwOuter.
+					// Criamos uma nova variável iwCloseBtn.
+					// Utilizando o método .next() do JQuery referenciamos o div seguinte ao div .gm-style-iw.
+					// É este div que agrupa os elementos do botão fechar.
+					var iwCloseBtn = iwOuter.next();
+					var indicador = iwOuter.prev();
+
+					indicador.css({
+						zIndex: 1
+					})
+
+					// Aplica o efeito desejado ao botão fechar
+					iwCloseBtn.css({
+					  opacity: '1', // por padrão o botão fechar tem uma opacidade de 0.7
+					  right: '28px', 
+					  top: '8px', // reposicionamento do botão
+					  border: '1px solid #48b5e9', // aumento da borda do botão e nova cor
+					  'border-radius': '13px', // efeito circular
+					  'box-shadow': '0 0 5px #3990B9' // efeito 3D para salientar o botão
+					});
+
+					// A API aplica automaticamente 0.7 de opacidade ao botão após o evento mouseout.
+					// Esta função reverte esse evento para o valor desejado.
+					iwCloseBtn.mouseout(function(){
+					  $(this).css({opacity: '1'});
+					});
+
+					var btnApoiar = iwOuter.find('.apoiar > p');
+					var imgApoiar = iwOuter.find('.apoiar > img');
+
+					btnApoiar.on('click', function(event) {
+
+						event.preventDefault();
+
+						if (!$(this).hasClass('apoiado'))
+						{
+							// mapService.apoiar($scope.postMin.idPublicacao).then(function (response) {
+
+								$(this).addClass('apoiado');
+
+								$(this).html('apoiado');
+
+								imgApoiar.removeAttr('src');
+								imgApoiar.attr('src', '/img/logo-apoiado.png');
+
+							// }, function (response) {
+
+							// });
+						}
+						else
+						{
+							// mapService.apoiar($scope.postMin.idPublicacao).then(function (response) {
+
+								$(this).removeClass('apoiado');
+
+								$(this).html('apoiar');
+
+								imgApoiar.removeAttr('src');
+								imgApoiar.attr('src', '/img/logo-apoiar.png');
+
+							// }, function (response) {
+
+							// });
+						}
+						
+
+					});
+
+				});
+
+			}, function(response) {
+
+			});
 	      	
 		
 	}
 
 	apoiar = function(idPublicacao) {
-		mapService.apoiar(idPublicacao).then(function(response) {
+		// mapService.apoiar(idPublicacao).then(function(response) {
 
-		}, function() {
+		// }, function() {
 
-		});
-
-	}
+		// });
+debugger;
+		alert("apoiou!");
+	}	
 
 }]);
